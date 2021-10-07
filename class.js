60e067d241f29ef67b08dd899b73db8c224a31f9
Pikmin1211 <joefarr1211@yahoo.ca>
2021-10-07 17:43:27 -0400

    file in and out

--- class.js
+++ class.js
@@ -1,3 +1,15 @@
-function testFunc(){
-	alert("ok");
+// Setting up button functions 
+document.getElementById("exportClassFile").onclick = exportClassFile_OnClick;
+document.getElementById("importClassFile").onchange = importClassFile_OnChange;
+
+//
+function exportClassFile_OnClick(){
+    SaveAsFile("text","filename.txt","text/plain;charset=utf-8");
 }
+
+//
+function importClassFile_OnChange(){
+	var file = document.getElementById("importClassFile").files[0];
+	alert(file.name);
+}
+
---
+++ file.js
@@ -0,0 +1,8 @@
+function SaveAsFile(t,f,m) {
+    try {
+        var b = new Blob([t],{type:m});
+        saveAs(b, f);
+    } catch (e) {
+        window.open("data:"+m+"," + encodeURIComponent(t), '_blank','');
+    }
+};
